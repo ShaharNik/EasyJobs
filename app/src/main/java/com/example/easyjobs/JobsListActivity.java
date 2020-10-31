2022223fb9d6d66127f6ea075b371c1077ab1b2a
Yosef <yoseftwito5299@gmail.com>
2020-11-01 00:21:16 +0200

    Added new Activities.

--- app/src/main/java/com/example/easyjobs/JobsListActivity.java
+++ app/src/main/java/com/example/easyjobs/JobsListActivity.java
@@ -5,13 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class JobsListActivity extends AppCompatActivity {
+
+    private Button jobProfile;
+    private Button postJob;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jobs_list);
+
+        jobProfile = findViewById(R.id.jobList_to_JobProfile);
+        jobProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToJobProfile();
+            }
+        });
+
+        postJob = findViewById(R.id.jobList_to_PostJob);
+        postJob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToPostJob();
+            }
+        });
+    }
+
+    public void moveToJobProfile(){
+        Intent i = new Intent(JobsListActivity.this, JobProfileActivity.class);
+        startActivity(i);
+    }
+
+    public void moveToPostJob(){
+        Intent i = new Intent(JobsListActivity.this, PostJobActivity.class);
+        startActivity(i);
     }
 
     public void moveToMain(View v){
--- app/src/main/java/com/example/easyjobs/profListActivity.java
+++ app/src/main/java/com/example/easyjobs/profListActivity.java
@@ -5,13 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class profListActivity extends AppCompatActivity {
+
+    private Button profProfile;
+    private Button postProf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prof_list);
+
+        profProfile = findViewById(R.id.profList_to_profProfile);
+        profProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToProfProfile();
+            }
+        });
+
+        postProf = findViewById(R.id.profList_to_PostProf);
+        postProf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToPostProf();
+            }
+        });
+    }
+
+    public void moveToProfProfile(){
+        Intent i = new Intent(profListActivity.this, ProfProfileActivity.class);
+        startActivity(i);
+    }
+
+    public void moveToPostProf(){
+        Intent i = new Intent(profListActivity.this, PostProfActivity.class);
+        startActivity(i);
     }
 
     public void moveToMain(View v){
