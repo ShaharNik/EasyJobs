68b0132fba240dc02ce80362cfddc7b22ec04a9b
Shahar Niknazar <57103646+ShaharNik@users.noreply.github.com>
2020-11-08 17:10:06 +0200

    Improve Generator
    Generates also A-Z

--- app/src/main/java/com/example/easyjobs/idGenerator.java
+++ app/src/main/java/com/example/easyjobs/idGenerator.java
@@ -7,12 +7,18 @@ public class idGenerator {
         char chars;
         int nums;
         int numOrChar;
+        int capitalLetter;
         for (int i = 0; i < 32; i++)
         {
             numOrChar = (int)(Math.random() * 2); //Generate number: 0 or 1
             if (numOrChar == 0)
             {
-                chars = (char)((Math.random()*(122-97+1)) + 97); // 97 - 122 Generate char 'a'-'z'
+                capitalLetter = (int)(Math.random() * 2); //Generate number: 0 or 1
+                if (capitalLetter == 0)
+                    chars = (char)((Math.random()*(122-97+1)) + 97); // 97 - 122 Generate char 'a'-'z'
+                else
+                    chars = (char)((Math.random()*(90-65+1)) + 65); // 65 - 90 Generate char 'A'-'Z'
+
                 token += chars;
             }
             else
