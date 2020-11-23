70ee66bb089a5782739c77da4de2b55d3c746cd5
yosef <yoseftwito5299@gmail.com>
2020-11-23 17:24:07 +0200

    Modified logo to universal size.

--- app/src/main/java/com/example/easyjobs/Activities/MainActivity.java
+++ app/src/main/java/com/example/easyjobs/Activities/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.easyjobs.R;
@@ -21,53 +24,69 @@ public class MainActivity extends AppCompatActivity {
     private TextView loginT;
     private FirebaseAuth mAuth; // For User Email & Password authentication
 
+    private ImageView logo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         findViews();
+        setLogoSize();
         activateButtonsAndViews();
         logedInModifier();
     }
 
     @Override
-    protected void onResume()
-    {
+    protected void onResume() {
         super.onResume();
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
-        if (user != null) // logged in
-        {
+        if (user != null){ // logged in
             String disp_name = user.getDisplayName();
             welcomeText.setText("Hello, " + disp_name + " :)");
             loginT.setText("פרופיל");
         }
-        else
-        {
+        else{
             welcomeText.setText("Hello, Guest :)");
             loginT.setText("התחבר/הירשם");
         }
     }
 
     private void findViews(){
+        logo = findViewById(R.id.imageView);
         welcomeText = findViewById(R.id.WelcomeText);
         jobListB = findViewById(R.id.button_mainto_joblist);
         proListB = findViewById(R.id.button_mainto_proflist);
         loginT = findViewById(R.id.button_mainto_login);
     }
 
+    private void setLogoSize(){
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        int screenHeight = displayMetrics.heightPixels;
+        int screenWidth = displayMetrics.widthPixels;
+
+        int imgWidth = (int) (screenWidth * 1);
+        int imgHeight = (int) (screenHeight * 0.45);
+
+        logo.getLayoutParams().height = imgHeight;
+        logo.getLayoutParams().width = imgWidth;
+    }
+
     private void activateButtonsAndViews(){
         loginT.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 mAuth = FirebaseAuth.getInstance();
                 FirebaseUser user = mAuth.getCurrentUser();
-                if (user == null) // logged in
+                if (user == null) { // logged in
                     moveToLoginActivity();
-                else
+                }
+                else {
                     moveToProfileActivity();
+                }
             }
         });
 
@@ -89,14 +108,12 @@ public class MainActivity extends AppCompatActivity {
     private void logedInModifier(){
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
-        if (user != null) // user is logged in
-        {
+        if (user != null){ // user is logged in
             String disp_name = user.getDisplayName();
             welcomeText.setText("Hello, " + disp_name + " :)");
             loginT.setText("פרופיל");
         }
-        else
-        {
+        else{
             welcomeText.setText("Hello, Guest :)");
             loginT.setText("Login/Register");
         }
@@ -117,8 +134,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(i);
     }
 
-    private void moveToProfileActivity() // **To be continued** //
-    {
+    private void moveToProfileActivity(){
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
         Intent i = new Intent(MainActivity.this, UserProfileActivity.class);
