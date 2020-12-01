ec7bd18ecb16f4b5bb30bb3e724e63cc28075761
YosefTwito <57401163+YosefTwito@users.noreply.github.com>
2020-12-01 21:18:12 +0200

    admin_edit_post activity.

--- app/src/main/java/com/example/easyjobs/Activities/AdminEditPostActivity.java
+++ app/src/main/java/com/example/easyjobs/Activities/AdminEditPostActivity.java
@@ -1,5 +1,6 @@
 package com.example.easyjobs.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,9 +10,21 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.example.easyjobs.Objects.Category;
 import com.example.easyjobs.Objects.Prof;
 import com.example.easyjobs.Objects.User;
 import com.example.easyjobs.R;
+import com.example.easyjobs.dataBase.FirebaseDBCategories;
+import com.example.easyjobs.dataBase.FirebaseDBProfs;
+import com.example.easyjobs.utils.Validator;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdminEditPostActivity extends AppCompatActivity {
 
@@ -20,12 +33,18 @@ public class AdminEditPostActivity extends AppCompatActivity {
     private TextView rateText;
     private EditText descText;
     private EditText catText;
+    private Button spinner;
     private EditText locText;
     private TextView phoneText;
     private Button approveChanges;
+    private MaterialDialog md;
 
     private User user;
     private Prof prof;
+    List<Category> oldCatList;
+    List<String> catList;
+
+    private boolean changedIt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +53,12 @@ public class AdminEditPostActivity extends AppCompatActivity {
 
         user = (User) getIntent().getSerializableExtra("User");
         prof = (Prof) getIntent().getSerializableExtra("Prof");
+        oldCatList = new ArrayList<Category>();
+        catList = new ArrayList<String>();
 
         findViews();
         inputTempData();
+        setUpSpinner();
         activateButtons();
     }
 
@@ -46,6 +68,7 @@ public class AdminEditPostActivity extends AppCompatActivity {
         rateText = findViewById(R.id.ratingEP);
         descText = findViewById(R.id.descriptionEP);
         catText = findViewById(R.id.catEP);
+        spinner = findViewById(R.id.pickCategoryEditProf);
         locText = findViewById(R.id.locationEP);
         phoneText = findViewById(R.id.phoneEP);
         approveChanges = findViewById(R.id.changeProfDetails);
@@ -54,12 +77,58 @@ public class AdminEditPostActivity extends AppCompatActivity {
     private void inputTempData() {
         namesText.setText(user.getFirstName() + " " + user.getLastName());
         rateText.setText(user.getRating()+ " ("+ user.getRatingsAmount()+")");
-
         descText.setText(prof.getDesc());
-        catText.setText("");
-        locText.setText(prof.getLocation());
 
+        DatabaseReference catDR;
+        for (int i=0; i<prof.getCategory().size(); i++){
+            final int x = i;
+            catDR = FirebaseDBCategories.getCatByID("\"" + prof.getCategory().get(i) + "\"");
+            catDR.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    Category c = snapshot.getValue(Category.class);
+                    oldCatList.add(c);
+                    catText.setText(catText.getText().toString() + c.getCat_name());
+                    if (x < prof.getCategory().size() - 1){
+                        catText.setText(catText.getText().toString() + ", ");
+                    }
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) { }
+            });
+        }
+
+        locText.setText(prof.getLocation());
         phoneText.setText(user.getPhoneNumber());
+    }
+
+    private void setUpSpinner(){
+        DatabaseReference dr = FirebaseDBCategories.getAllCat();
+        dr.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ArrayList<Category> items = new ArrayList<>();
+                for(DataSnapshot category : snapshot.getChildren()){
+                    Category c = category.getValue(Category.class);
+                    items.add(c);
+                }
+                md = new MaterialDialog.Builder(AdminEditPostActivity.this).title("בחר קטגוריות").items(items).itemsCallbackMultiChoice(null, new MaterialDialog.ListCallbackMultiChoice() {
+                    @Override
+                    public boolean onSelection(MaterialDialog dialog, Integer[] which, CharSequence[] text) {
+                        for (Integer i : which) {
+                            catList.add(items.get(i).getCategory_id());
+                        }
+                        return true;
+                    }
+                }).positiveText("אישור").build();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                for (Category c : oldCatList){
+                    catList.add(c.getCat_name());
+                }
+            }
+        });
     }
 
     private void activateButtons() {
@@ -67,6 +136,31 @@ public class AdminEditPostActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 AdminEditPostActivity.super.onBackPressed();
+            }
+        });
+
+        spinner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                md.show();
+            }
+        });
+
+        approveChanges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changedIt = true;
+                if(!Validator.ValidateDescription(descText.getText().toString())){
+                    changedIt = false;
+                    descText.setError("תיאור לא טוב");
+                }
+                if(!Validator.ValidateLocation(locText.getText().toString())){
+                    changedIt = false;
+                    locText.setError("מיקום לא טוב");
+                }
+                if(changedIt){
+                    FirebaseDBProfs.EditProf(prof.getProf_ID(), user.getUser_ID(), descText.getText().toString(), catList, locText.getText().toString());
+                }
             }
         });
     }
