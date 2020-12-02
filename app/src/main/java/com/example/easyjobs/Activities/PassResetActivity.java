b38b23207fcf8f6924ed4dc3b4503442a389a560
YosefTwito <57401163+YosefTwito@users.noreply.github.com>
2020-12-02 17:28:23 +0200

    pass Reset init

---
+++ app/src/main/java/com/example/easyjobs/Activities/PassResetActivity.java
@@ -0,0 +1,16 @@
+package com.example.easyjobs.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.easyjobs.R;
+
+public class PassResetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pass_reset);
+    }
+}
